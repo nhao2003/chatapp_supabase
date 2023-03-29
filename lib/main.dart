d75ae5323a6fdabe0ee5bcf789aec984e2cbd87a
nhao2003 <nhathaodev@gmail.com>
2023-03-29 14:59:44 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:chatapp_supabase/pages/splash_page.dart';
+import 'package:chatapp_supabase/utils/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Supabase.initialize(
+    // TODO: Replace credentials with your own
+    url: 'SUPABASE_URL',
+    anonKey: 'SUPABASE_ANON_KEY',
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My Chat App',
+      theme: appTheme,
+      home: const SplashPage(),
+    );
+  }
+}
